7f94cb55d6fa5ed20f99754ac4e1308cbf936c20
ponnusherbal <ponnusherbal@gmail.com>
2024-06-28 16:57:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const container3 = document.getElementById("third");
+container3.addEventListener("click", () => 
+{
+    window.location.href = "third.html";
+})
+
+const container2 = document.getElementById("second");
+container2.addEventListener("click", () => 
+{
+    window.location.href = "second.html";
+})
+
+const container1 = document.getElementById("first");
+container1.addEventListener("click", () => 
+{
+    window.location.href = "first.html";
+})
+
+const container4 = document.getElementById("forth");
+container4.addEventListener("click", () => 
+{
+    window.location.href = "forth.html";
+})
+
+const container5 = document.getElementById("fifth");
+container5.addEventListener("click", () => 
+{
+    window.location.href = "fifth.html";
+})
+
+const container6 = document.getElementById("sixth");
+container6.addEventListener("click", () => 
+{
+    window.location.href = "sixth.html";
+})
